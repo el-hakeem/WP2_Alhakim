74ba5d4249fe06cf0dfb9812c157fb853ecb9a82
el-hakeem <naufalalhakim931@gmil.com>
2024-04-24 19:52:26 +0700

    Upload Tugas pertemuan 3membuat Template dan Setting Base URL

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Naufal</li>
+        <li>Nama Belakang : Alhakim</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Kp. Lubang Buaya Kec.Setu </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cirebon</li>
+    </ul>
+    <h4>Hal yang disukai</h4>
+    <ul type="square">
+        <li>Kucing Munchkin</li>
+        <li>Kucing British</li>
+    </ul >
+</section>
